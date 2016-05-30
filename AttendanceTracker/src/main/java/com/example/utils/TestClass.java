517c7fdbb40edcc71969f4658ff527d2feffbfc5
MaheshaMB <mbmahesha47@gmail.com>
2016-05-30 13:41:04 +0530

    updated TestClass in eclipse.

--- AttendanceTracker/src/main/java/com/example/utils/TestClass.java
+++ AttendanceTracker/src/main/java/com/example/utils/TestClass.java
@@ -4,6 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World...");
+		String str = "New World...";
+		System.out.println("Print STR===>" + str);
 	}
 
 }
